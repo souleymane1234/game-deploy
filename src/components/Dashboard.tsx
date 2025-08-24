e75db9e350214350e5a7b9a671d1014a325b77a8
souleymane1234 <konatesouleymane42@gmail.com>
2025-08-24 12:54:51 +0000

    first

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -190,6 +190,20 @@ const Dashboard: React.FC = () => {
           <StatLabel>Support</StatLabel>
         </StatCard>
       </StatsSection>
+      <StatsSection>
+        <StatCard>
+          <StatNumber>2</StatNumber>
+          <StatLabel>Jeux Disponibles</StatLabel>
+        </StatCard>
+        <StatCard>
+          <StatNumber>100%</StatNumber>
+          <StatLabel>Fair Play</StatLabel>
+        </StatCard>
+        <StatCard>
+          <StatNumber>24/7</StatNumber>
+          <StatLabel>Support</StatLabel>
+        </StatCard>
+      </StatsSection>
     </DashboardContainer>
   );
 };
